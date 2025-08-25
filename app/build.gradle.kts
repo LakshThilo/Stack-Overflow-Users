39dc347d96f334fcea0dd1b5cb1e7e97185420af
lakwijewaradane <lbataranage@cbs.site>
2025-08-25 09:58:26 +0100

    initial commit - add gradle dependencies

---
+++ app/build.gradle.kts
@@ -0,0 +1,103 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    //id("org.jetbrains.kotlin.kapt")
+    alias(libs.plugins.kotlin.kapt)
+    alias(libs.plugins.hilt.android)
+}
+
+android {
+    namespace = "com.blpw.pixelex"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.blpw.pixelex"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.composeCompiler.get()
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    packagingOptions {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.coroutines)
+
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.material)
+    implementation(libs.androidx.compose.material.icons)
+    implementation(libs.androidx.compose.foundation)
+    implementation(libs.androidx.compose.ui.tooling)
+    implementation(libs.androidx.material3)
+    implementation(libs.coil.compose2)
+
+    implementation(libs.navigation.compose)
+    implementation(libs.accompanist.pager)
+    implementation(libs.accompanist.pager.indicators)
+    implementation(libs.accompanist.system.ui)
+    implementation(libs.accompanist.swiperefresh)
+
+
+    // Retrofit
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.moshi)
+    implementation(libs.okhttp)
+
+    // Moshi
+    implementation(libs.moshi)
+    implementation(libs.moshi.kotlin)
+
+    // gson
+    implementation(libs.retrofit.gson)
+
+    // Hilt
+    implementation(libs.dagger.hilt.android)
+    kapt(libs.dagger.hilt.compiler)
+    kapt(libs.androidx.hilt.compiler)
+    implementation(libs.androidx.hilt.navigation.compose)
+
+    // Room
+    implementation(libs.room.runtime)
+    implementation(libs.room.ktx)
+    kapt(libs.room.compiler)
+
+    // Testing
+    testImplementation(libs.junit)
+    testImplementation(libs.truth)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
---
+++ app/src/main/java/com/blpw/pixelex/ui/theme/Color.kt
@@ -0,0 +1,67 @@
+package com.blpw.pixelex.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val LightBlue = Color(0xFFBAC7FF)
+val lightGrey = Color(0xFFAAAAAA)
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val TypeNormal = Color(0xFFA8A77A)
+val TypeFire = Color(0xFFEE8130)
+val TypeWater = Color(0xFF6390F0)
+val TypeElectric = Color(0xFFF7D02C)
+val TypeGrass = Color(0xFF7AC74C)
+val TypeIce = Color(0xFF96D9D6)
+val TypeFighting = Color(0xFFC22E28)
+val TypePoison = Color(0xFFA33EA1)
+val TypeGround = Color(0xFFE2BF65)
+val TypeFlying = Color(0xFFA98FF3)
+val TypePsychic = Color(0xFFF95587)
+val TypeBug = Color(0xFFA6B91A)
+val TypeRock = Color(0xFFB6A136)
+val TypeGhost = Color(0xFF735797)
+val TypeDragon = Color(0xFF6F35FC)
+val TypeDark = Color(0xFF705746)
+val TypeSteel = Color(0xFFB7B7CE)
+val TypeFairy = Color(0xFFD685AD)
+
+val LightNormal = Color(0xFFD6D3B3)
+val LightFire = Color(0xFFF9C78B)
+val LightWater = Color(0xFFB3C9FF)
+val LightElectric = Color(0xFFFFF5B1)
+val LightGrass = Color(0xFFC9E4A4)
+val LightIce = Color(0xFFD6E7E9)
+val LightFighting = Color(0xFFEB9998)
+val LightPoison = Color(0xFFD7A7DC)
+val LightGround = Color(0xFFF1E3B3)
+val LightFlying = Color(0xFFD6CCFF)
+
+val LightCoral      = Color(0xFFFFB3A1) // warm coral
+val LightTeal       = Color(0xFF9DE5DA) // aqua-teal
+val LightMustard    = Color(0xFFFFD36A) // golden mustard
+val LightPeriwinkle = Color(0xFFC3C0FF) // periwinkle blue
+val LightOlive      = Color(0xFFDDE59A) // olive green
+val LightPeach      = Color(0xFFFFC4AE) // peach
+val LightMint       = Color(0xFFB7F2CF) // mint green
+val LightTerracotta = Color(0xFFE8AB8F) // terracotta
+val LightSlate      = Color(0xFFBAC8D6) // blue-gray slate
+val LightMulberry   = Color(0xFFE7A8C8) // mulberry pink
+
+val SandYellow = Color(0xFFFFBD64)
+
+val MediumNormal = Color(0xFF9EA766)
+val MediumFire = Color(0xFFE0852E)
+val MediumWater = Color(0xFF4A73D9)
+val MediumElectric = Color(0xFFE2CB24)
+val MediumGrass = Color(0xFF699D3F)
+val MediumIce = Color(0xFF77B5C6)
+val MediumFighting = Color(0xFFAC4140)
+val MediumPoison = Color(0xFF9B4AA3)
+val MediumGround = Color(0xFFCEB85D)
+val MediumFlying = Color(0xFF866BF9)
